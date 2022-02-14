0ce40e230ccc3b8fd86b03ea5573d34498064d66
Omar Loudghiri <omar.loudghiri@uwc-usa.org>
2022-02-13 22:30:33 -0500

    Implemented the ImageStore interface

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/data/inject/DataBindings.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/inject/DataBindings.java
@@ -1,7 +1,9 @@
 package com.learningmachine.android.app.data.inject;
 
 import com.learningmachine.android.app.data.store.CertificateStore;
+import com.learningmachine.android.app.data.store.ImageStore;
 import com.learningmachine.android.app.data.store.SQLiteCertificateStore;
+import com.learningmachine.android.app.data.store.SQLiteImageStore;
 
 import javax.inject.Singleton;
 
@@ -14,4 +16,5 @@ interface DataBindings {
     @Binds
     @Singleton
     CertificateStore bindCertificateStore(SQLiteCertificateStore certStore);
+    ImageStore bindImageStore(SQLiteImageStore imageStore);
 }
--- LearningMachine/app/src/main/java/com/learningmachine/android/app/data/store/ImageStore.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/store/ImageStore.java
@@ -1,73 +1,10 @@
 package com.learningmachine.android.app.data.store;
 
-import android.content.Context;
-import android.util.Base64;
 
-import com.learningmachine.android.app.util.ImageUtils;
-import com.learningmachine.android.app.util.StringUtils;
+import dagger.Provides;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+public interface ImageStore extends DataStore {
+     @Provides
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import timber.log.Timber;
-
-@Singleton
-public class ImageStore implements DataStore {
-
-    private final Context mContext;
-
-    @Inject
-    public ImageStore(Context context) {
-        mContext = context;
-    }
-
-    /**
-     * @param uuid     Issuer url
-     * @param jsonData Image data
-     * @return true if the image was written to file successfully
-     */
-    public boolean saveImage(String uuid, String jsonData) {
-        if (StringUtils.isEmpty(uuid) || StringUtils.isEmpty(jsonData)) {
-            return false;
-        }
-
-        String filename = ImageUtils.getImageFilename(uuid);
-        if (StringUtils.isEmpty(filename)) {
-            return false;
-        }
-
-        String imageData = ImageUtils.getImageDataFromJson(jsonData);
-        if (StringUtils.isEmpty(imageData)) {
-            return false;
-        }
-
-        boolean success = false;
-
-        try (FileOutputStream fileOutputStream = mContext.openFileOutput(filename, Context.MODE_PRIVATE)) {
-            byte[] decodedString = Base64.decode(imageData, Base64.DEFAULT);
-            fileOutputStream.write(decodedString);
-            fileOutputStream.flush();
-            success = true;
-        } catch (FileNotFoundException e) {
-            Timber.e(e, "Unable to open file");
-        } catch (IOException e) {
-            Timber.e(e, "Unable to write to file");
-        }
-
-        return success;
-    }
-
-    @Override
-    public void reset() {
-        String[] fileList = mContext.fileList();
-        for (String file : fileList) {
-            if (file.contains(".png")) {
-                mContext.deleteFile(file);
-            }
-        }
-    }
+     boolean saveImage(String uuid, String jsonData);
 }
