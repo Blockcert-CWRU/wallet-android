d18f88bc32a5e5486cbba00c12f7b4b5531f8507
rtatton <rdt17@protonmail.com>
2022-04-23 15:18:46 -0400

    Use DI; add headers
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- LearningMachine/app/src/main/java/com/learningmachine/android/app/data/store/pda/PDACertificateStoreService.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/store/pda/PDACertificateStoreService.java
@@ -4,33 +4,30 @@ import com.learningmachine.android.app.data.cert.BlockCert;
 import com.learningmachine.android.app.data.model.CertificateRecord;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 import rx.Observable;
 
 public interface PDACertificateStoreService {
 
-    @GET("wallet/certs")
-    Observable<List<CertificateRecord>> load(
-            @Header("Content-Type") String contentType,
-            @Header("x-auth-token") String authToken
-    );
+    @GET("/certs")
+    Observable<List<CertificateRecord>> load(@Header("x-auth-token") String authToken);
 
-    @POST("wallet/certs/")
-    Observable<Void> save(
-            @Header("Content-Type") String contentType,
-            @Header("x-auth-token") String authToken,
-            @Body BlockCert cert);
+    @POST("/certs")
+    @Headers({"Content-Type: application/json"})
+    Observable<Void> save(@Body BlockCert cert, @Header("x-auth-token") String authToken);
 
     @DELETE("/certs")
     Observable<Void> delete(
-            @Query("records") String certId,
+            @QueryMap Map<String, String> records,
             @Path("hatName") String hatName,
             @Header("x-auth-token") String authToken);
 }
--- LearningMachine/app/src/main/java/com/learningmachine/android/app/data/store/pda/PDAIndexService.java
+++ LearningMachine/app/src/main/java/com/learningmachine/android/app/data/store/pda/PDAIndexService.java
@@ -4,6 +4,7 @@ import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
@@ -12,20 +13,24 @@ import rx.Observable;
 public interface PDAIndexService {
 
     @GET("/index")
-    Observable<PDAIndex> get(@Path("hatName") String hatName, @Header("x-auth-token") String authToken);
+    Observable<PDAIndex> get(
+            @Path("hatName") String hatName, @Header("x-auth-token") String authToken);
 
     @POST("/index")
+    @Headers({"Content-Type: application/json"})
     Observable<Void> create(
             @Body PDAIndex index,
             @Path("hatName") String hatName,
             @Header("x-auth-token") String authToken);
 
     @PUT("/index")
+    @Headers({"Content-Type: application/json"})
     Observable<Void> update(
             @Body PDAIndex index,
             @Path("hatName") String hatName,
             @Header("x-auth-token") String authToken);
 
     @DELETE("/index")
-    Observable<Void> delete(@Path("hatName") String hatName, @Header("x-auth-token") String authToken);
+    Observable<Void> delete(
+            @Path("hatName") String hatName, @Header("x-auth-token") String authToken);
 }
